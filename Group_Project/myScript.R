ba4846d314e8eda64c38ba545b86c4688c518f7f
JamesChartraw <82541573+JamesChartraw@users.noreply.github.com>
2022-05-06 20:09:34 -0500

    Added missing files from 5/3

--- Group_Project/myScript.R
+++ Group_Project/myScript.R
@@ -1,6 +1,24 @@
 load("Gender_UW_Schools.Rdata")
-load("All_Aid_SOC_2015-2019.Rdata")
-load("All_Aid_NonURM_2015-2019.Rdata")
-load("All_Aid_URM_2015-2019.Rdata")
-load("All_Aid_URM_UWO_2015-2019.Rdata")
-load("All_Aid_NonURM_UWO_2015-2019.RData")
+load("UWO_Gender_degrees.RData")
+
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
+UWO_Females <- subset(Gender_1973_2021_UW_Schools, select=c(Year, `UW-Oshkosh Female`))
+
+UWO_Males <- subset(Gender_1973_2021_UW_Schools, select=c(Year, `UW-Oshkosh Male`))
+
+UWO_Female_Degrees <- subset(UWO_Gender_Degrees, Gender=="Female", select = c(Year, Gender, `Degrees Conferred`))
+
+UWO_Male_Degrees <- subset(UWO_Gender_Degrees, Gender=="Male", select = c(Year, Gender, `Degrees Conferred`))
+
+## Enrollment by Gender from 1974 to 2021
+ggplot(data = UWO_Gender_Degrees, mapping = aes(x = Year, y = UWO_Gender_Degrees$`Degrees Conferred`)) +
+  geom_line(aes(group = Gender))
+
+Male_Pop_Degrees <- left_join(UWO_Male_Degrees, UWO_Males)
+
+## Work in Progress! Trying to plot population of males and degrees earned by males by year.
+ggplot(data = Male_Pop_Degrees, mapping = aes(x = `UW-Oshkosh Male`, y = `Degrees Conferred`)) +
+  geom_point(aes(group = Year))
